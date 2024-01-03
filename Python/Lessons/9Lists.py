380fc70cf088f5ea92456f738892bde574579b1b
DiegoMars <131321814+DiegoMars@users.noreply.github.com>
2024-01-02 22:58:57 -0800

    Create 9Lists.py

---
+++ Python/Lessons/9Lists.py
@@ -0,0 +1,4 @@
+#   List  = [] ordered and changeable. Duplicates OK
+#   Set   = {} unordered and immutable, but Add/Remove OK. NO duplicates
+#   Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+#   You can use print(help(__)) to find functions used for those specific lists
