c2e80e18613719d9a6addddd50e1d0d3ed0a7eae
DiegoMars <131321814+DiegoMars@users.noreply.github.com>
2024-03-16 19:53:12 -0700

    Day 1 (33:05)

---
+++ astro/test/src/content/config.ts
@@ -0,0 +1,16 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+// 2. Define your collection(s)
+const postsCollection = defineCollection({ 
+    schema: z.object({
+        author: z.string(),
+        date: z.string(),
+        images: z.string(),
+        title: z.string()
+    })
+ });
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  posts: postsCollection,
+};
