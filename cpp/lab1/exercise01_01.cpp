89ca47214df67b7a11d9b3a09ddfd373f58098bf
DiegoMars <131321814+DiegoMars@users.noreply.github.com>
2024-06-12 16:53:18 -0700

    Finished Lab 1

---
+++ cpp/lab1/exercise01_01.cpp
@@ -0,0 +1,19 @@
+// Diego Martinez
+// Created: June 12, 2024
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Welcome to C++" << endl 
+    << "Welcome to Computer Science" << endl 
+    << "Programming is fun." << endl;
+
+    return 0;
+}
+
+// Output
+/*
+    Welcome to C++
+    Welcome to Computer Science
+    Programming is fun.
+*/
---
+++ cpp/lab1/exercise01_09.cpp
@@ -0,0 +1,17 @@
+// Diego Martinez
+// Created: June 12, 2024
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "The area is " << (4.5*7.9) << endl;
+    cout << "The perimeter is " << (2*(4.5+7.9)) << endl;
+
+    return 0;
+}
+
+// Output
+/*
+    The area is 35.55
+    The perimeter is 24.8
+*/
