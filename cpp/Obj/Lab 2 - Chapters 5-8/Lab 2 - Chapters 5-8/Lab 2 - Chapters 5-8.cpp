93d52e33c5a796f715f0aeabd56edf96d7569327
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2024-11-04 21:13:06 -0800

    Lab 2 added

---
+++ cpp/Obj/Lab 2 - Chapters 5-8/Lab 2 - Chapters 5-8/Lab 2 - Chapters 5-8.cpp
@@ -0,0 +1,35 @@
+﻿// Diego Martinez
+// Created: Nov 3, 2024
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Initialize array
+    bool lockers[100];
+    for (int i = 0; i < 100; i++) {
+        lockers[i] = false;
+    }
+
+    // Goes through lockers
+    for (int i = 0; i < 100; i++) {
+        for (int j = i; j < 100; j = j + i + 1) {
+            lockers[j] = !lockers[j];
+        }
+    }
+
+    // Prints what lockers are open
+    for (int i = 0; i < 100; i++) {
+        if (lockers[i]) {
+            cout << i+1 << " ";
+        }
+    }
+
+    return 0;
+}
+
+// Output
+/*
+    1 4 9 16 25 36 49 64 81 100
+*/
