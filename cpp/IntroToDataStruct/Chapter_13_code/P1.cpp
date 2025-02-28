634dd7e8c9312308b08a00eab844995c10cc6735
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2025-02-28 00:13:44 -0800

    Finished CH 13 P1

--- cpp/IntroToDataStruct/Chapter_13_code/P1.cpp
+++ cpp/IntroToDataStruct/Chapter_13_code/P1.cpp
@@ -4,3 +4,50 @@
 // of the argument that is the greater of the two. Design a simple driver program that
 // demonstrates the templates with various data types.
 
+#include<iostream>
+using namespace std;
+
+template <class T>
+T minimum(T a, T b){
+    if (a > b){
+        return a;
+    } else {
+        return b;
+    }
+}
+template <class T>
+T maximum(T a, T b){
+    if (a < b){
+        return a;
+    } else {
+        return b;
+    }
+}
+
+int main(){
+    // Test with int arguments.
+    int num1 = 5;
+    int num2 = 3;
+    cout << "minimum of 5, 3 is: " ;
+    cout << minimum(num1, num2) << endl;
+    cout << "maximum of 5, 3 is: " ;
+    cout << maximum(num1, num2) << endl;
+
+    // Test with double arguments.
+    double num3 = 5.5;
+    double num4 = 3.5;
+    cout << "minimum of 5.5, 3.5 is: " ;
+    cout << minimum(num3, num4) << endl;
+    cout << "maximum of 5.5, 3.5 is: " ;
+    cout << maximum(num3, num4) << endl;
+    
+    // Test with string arguments.
+    string hello = "hello";
+    string hi = "hi";
+    cout << "minimum of \"hello\" and \"hi\" is: ";
+    cout << minimum(hello, hi) << endl;
+    cout << "maximum of \"hello\" and \"hi\" is: ";
+    cout << maximum(hello, hi) << endl;
+
+    return 0;
+}
