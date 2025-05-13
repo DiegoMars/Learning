8d3ae5669e16bca891f0649a872ee1d6a24594e3
MarsCarbon <131321814+DiegoMars@users.noreply.github.com>
2025-05-13 10:43:30 -0700

    Finished doing some indenting testing

--- nvim/testing.py
+++ nvim/testing.py
@@ -1,3 +1,13 @@
 # Doing some testing for nvims stuff
 # Lowkey real nice so far
 print("Hello from nvim")
+
+
+def greet():
+    print("Hello from function")
+    i = 0
+    print(i)
+    # Looks like the indenting stuff works
+
+
+greet()
