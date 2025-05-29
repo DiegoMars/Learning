9219137d56fb004f77354eff654c3197a7e09abd
MarsCarbon <131321814+DiegoMars@users.noreply.github.com>
2025-05-29 08:43:07 -0700

    Finished encryption

--- discreteMathematics/finalOpt3/sss.py
+++ discreteMathematics/finalOpt3/sss.py
@@ -3,6 +3,8 @@
 
 # Imports
 import math
+import random
+
 
 ### Encrypt ###
 # 1) Input a secret, some threshold K, and some number of shares N
@@ -15,9 +17,10 @@ import math
 #    up with threshold, but lets call it modulus)
 # 4) Populate random coefficients, making sure that it is less than the prime
 #    number, then get the number of shares
-# 5) Output the shares (Idk what else can be outputted)
+# 5) Output the shares by getting a random x and get the respective y by
+#    multiplying each coefficient by x, add them together, and add the constant
 
-
+# Checks if number is prime
 def prime(n):
     for i in range(2, int(math.sqrt(abs(n))) + 1):
         if (n % i == 0):
@@ -25,6 +28,7 @@ def prime(n):
     return True
 
 
+# Gets prime number bigger than number
 def greaterPrime(number):
     primeNumber = number+1
     while (not prime(primeNumber)):
@@ -32,11 +36,20 @@ def greaterPrime(number):
     return primeNumber
 
 
+def generateY(polynomial, x, modulus):
+    y = polynomial[0]  # Adds constant
+    for i in range(len(polynomial)-1):
+        y += x**(i+1)
+    return (y % modulus)  # Keeps the y under modulus
+
+
 def encrpt(secret, k, n):
     if (k > n):
         print("ERROR: Threshold greater than the number of shares")
-        return ("Error")
+        exit(1)
     degree = k-1
+
+    # Grabs the prime number greater than the biggest input number
     if (secret >= k and secret >= n):
         tempMod = secret
     elif (k >= secret and k >= n):
@@ -45,6 +58,24 @@ def encrpt(secret, k, n):
         tempMod = n
     modulus = greaterPrime(tempMod)
 
+    # This represents the the coefficients and the constant
+    polynomial = [secret]
+    for i in range(degree):
+        polynomial.append(random.randint(1, modulus-1))
+
+    # Generates shares
+    shares = []
+    for i in range(n):
+        share = []
+        x = random.randint(1, 1000)
+        y = generateY(polynomial, x, modulus)
+        share.append(x)
+        share.append(y)
+        shares.append(share)
+    return (shares, modulus)
+
 
 ### Running Code ###
-encrpt(1234, 3, 5)
+shares, modulus = encrpt(65, 2, 4)
+print(shares)
+print(modulus)
