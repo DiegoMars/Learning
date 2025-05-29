95ecae0fe4e9f0f637feefd3df9f83240eb40090
MarsCarbon <131321814+DiegoMars@users.noreply.github.com>
2025-05-29 07:14:57 -0700

    I don't even know LMAO

--- JavaScript/basics/basics.js
+++ JavaScript/basics/basics.js
@@ -5,7 +5,7 @@
 
 let name = "John";
 var surname = "Doe"; 	// These 2 ways of initializing are valid, but "let" is
-			// used more I think, and var is older
+											// used more I think, and var is older
 name = "Diego" // Don't need an initializer to rename variables
 
 console.log(name); // Prints to terminal (or console idk the name)
@@ -60,3 +60,32 @@ let sum = function(a, b) {
 };
 */
 console.log( sum3(1, 2) ); // 3
+
+// Some array stuff
+console.log("Map demo below:")
+const arr = [1, 2, 3, 4, 5];
+const mappedArr = arr.map((num) => num + 1); // Arrow function
+																						 // map runs every element through
+																						 // through a specified function
+console.log(mappedArr); // Outputs [2, 3, 4, 5, 6]
+// Note that this does not override the original array
+
+// This demonstrates the filter function, which returns an array only if the
+// element mapped through the function is truw
+console.log("Filter demo below:")
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+const arr2 = [1, 2, 3, 4, 5];
+const oddNums = arr2.filter(isOdd);
+console.log(oddNums); // Outputs [1, 3, 5];
+console.log(arr2); // Outputs [1, 2, 3, 4, 5], original array is not affected
+
+// Demonstration of the reduce function
+console.log("Reduce demo below:")
+const arr3 = [1, 2, 3, 4, 5];
+const productOfAllNums = arr3.reduce((total, currentItem) => {
+  return total * currentItem;
+}, 1);
+console.log(productOfAllNums); // Outputs 120;
+console.log(arr3); // Outputs [1, 2, 3, 4, 5]
