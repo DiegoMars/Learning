72ac8dba84eb99944c67ae0bcc016c7f8dea8afd
DiegoMars <131321814+DiegoMars@users.noreply.github.com>
2026-03-09 17:08:18 -0700

    3/9/26 class code

--- JavaScript/comp333/types.js
+++ JavaScript/comp333/types.js
@@ -1,6 +1,22 @@
 // You can do ".load types.js" do load this in node
 
-// You know this works with both strings and numbers
+// This works with both strings and numbers
+// Statically typed languages vs dynamic type have the advantage since we already know what
+//      code needs to be run at compile time.
+// That being say, there are stuff that could be written in dynamically typed languages that
+//      you just can't in static. This add function is one of them
+
 function add(x, y) {
-	return x + y;
+    return x + y;
+}
+
+// This would be something else that can be written in js and not java, since java needs to
+// explicitly say what the return type is and stick to it. There would be ways around it with an
+// interface, but would be a lot more verbose.
+function doSomething(a) {
+    if (a < 3) {
+        return 0
+    } else {
+        return "foo";
+    }
 }
