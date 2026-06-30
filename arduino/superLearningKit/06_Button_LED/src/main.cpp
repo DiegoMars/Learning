1b8a2a0d366d118b93822c61804012576ec70cd5
DiegoMars <131321814+DiegoMars@users.noreply.github.com>
2026-06-30 13:24:15 -0700

    Added button states

--- arduino/superLearningKit/06_Button_LED/src/main.cpp
+++ arduino/superLearningKit/06_Button_LED/src/main.cpp
@@ -3,27 +3,44 @@
 int LED_PIN = 11;
 int IN_PIN = 7;
 int val;
-bool pressed;
-bool on;
+
+int state = HIGH;
+int reading;
+int previous = LOW;
+
+unsigned long time = 0;
+unsigned long debounce = 200UL; // UL means unsigned long
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(IN_PIN, INPUT);
+  Serial.begin(9600);
 }
 
+// void loop() {
+//   val = digitalRead(IN_PIN);
+//   if (val == LOW){
+//     digitalWrite(LED_PIN, LOW);
+//   } else {
+//     digitalWrite(LED_PIN, HIGH);
+//   }
+// }
+
 void loop() {
-  val = digitalRead(IN_PIN);
-  if (val == HIGH) {
-    if (pressed) {
-      pressed = false;
+  reading = digitalRead(IN_PIN);
+
+  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
+    if (state == HIGH) {
+      state = LOW;
+      Serial.println("Low");
     } else {
-      pressed = true;
+      state = HIGH;
+      Serial.println("High");
     }
-  } else {
-    if (pressed) {
-      digitalWrite(LED_PIN, HIGH);
-    } else {
-      digitalWrite(LED_PIN, LOW);
-    }
+    time = millis();
   }
+
+  digitalWrite(LED_PIN, state);
+
+  previous = reading;
 }
